920a3bb7caf07d8611fb79cb8674b7656747c7b8
Laercio Bernardo <laercio.bernardo@intrapoint.com>
2016-09-22 14:41:44 -0300

    First commit

---
+++ getBooks.js
@@ -0,0 +1,72 @@
+var request = require('request');
+var cheerio = require('cheerio');
+var fs = require('fs');
+var url = require('url');
+var exec = require('child_process').exec;
+var escapeStringRegexp = require('escape-string-regexp');
+
+var DOWNLOAD_DIR = "./downloads/";
+
+
+function readLines(input, func) {
+  var remaining = '';
+
+  input.on('data', function(data) {
+    remaining += data;
+    var index = remaining.indexOf('\n');
+    while (index > -1) {
+      var line = remaining.substring(0, index);
+      remaining = remaining.substring(index + 1);
+      func(line);
+      index = remaining.indexOf('\n');
+    }
+  });
+
+  input.on('end', function() {
+    if (remaining.length > 0) {
+      func(remaining);
+    }
+  });
+}
+
+function func(data) {
+  console.log('Make request to: ' + data);
+    request(data, function(err, resp, body){
+      if(err) return;
+
+      console.log("Status code: "+resp.statusCode);
+
+      var $ = cheerio.load(body);
+
+      $('div.links-download > a').each(function (index){
+        if(index < 3){
+          var link = $(this).attr('href');
+
+          // fs.appendFileSync('linksDownloads.txt', link + '\n');
+          download_file_wget(link);
+        }
+      }).text();
+    });
+
+}
+
+
+
+var download_file_wget = function(file_url) {
+    var urlParsed = escapeStringRegexp(encodeURI(file_url));
+
+    // extract the file name
+    var file_name = url.parse(urlParsed).pathname.split('/').pop();
+    // compose the wget command
+    var wget = 'wget -P ' + DOWNLOAD_DIR + ' ' + urlParsed;
+    console.log("executando o wget => ",wget);
+    // excute wget using child_process' exec function
+
+    var child = exec(wget, function(err, stdout, stderr) {
+        if (err) return;
+        else console.log(file_name + ' downloaded to ' + DOWNLOAD_DIR);
+    });
+};
+
+var input = fs.createReadStream('lelivros.txt');
+readLines(input, func);
---
+++ getUrls.js
@@ -0,0 +1,28 @@
+var request = require('request');
+var cheerio = require('cheerio');
+var fs = require('fs');
+
+var URL_PRINCIPAL = "http://lelivros.download/book/page/";
+var NUMERO_PAGINAS = 421;
+var urls = [];
+
+for (var i = 0; i <= NUMERO_PAGINAS; i++) {
+  var urlCompleta = [URL_PRINCIPAL,i,'/'].join('');
+  urls.push(urlCompleta);
+}
+
+
+urls.forEach(function(url){
+  request(url, function(err, resp, body){
+    if(err) console.log("Erro no request: ", err);
+
+    console.log("Status code: "+resp.statusCode);
+
+    var $ = cheerio.load(body);
+
+    $('ul.products > li').each(function (index){
+      var link = $(this).find('a').attr('href');
+      fs.appendFileSync('lelivros.txt', link + '\n');
+    }).text();
+  });
+});
