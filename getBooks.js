f7c96f6cccff3d6b73f412a56cde84848a05c253
Laercio Bernardo <laercio.bernardo@intrapoint.com>
2016-09-22 18:45:46 -0300

    Add errorLog

--- getBooks.js
+++ getBooks.js
@@ -13,6 +13,7 @@ function readLines(input, func) {
 
   input.on('data', function(data) {
     remaining += data;
+
     var index = remaining.indexOf('\n');
     while (index > -1) {
       var line = remaining.substring(0, index);
@@ -39,9 +40,8 @@ function func(data) {
       var $ = cheerio.load(body);
 
       $('div.links-download > a').each(function (index){
-        if(index < 3){
+        if(index < 1){
           var link = $(this).attr('href');
-
           // fs.appendFileSync('linksDownloads.txt', link + '\n');
           download_file_wget(link);
         }
@@ -63,8 +63,13 @@ var download_file_wget = function(file_url) {
     // excute wget using child_process' exec function
 
     var child = exec(wget, function(err, stdout, stderr) {
-        if (err) return;
-        else console.log(file_name + ' downloaded to ' + DOWNLOAD_DIR);
+        if (err){
+          var errLog = "Erro no download do livro : "+ file_url + " ** Erro: "+err;
+          fs.appendFileSync('erros.txt', errLog + '\n');
+          return;
+        } else {
+          console.log(file_url + ' downloaded to ' + DOWNLOAD_DIR);
+        }
     });
 };
 
